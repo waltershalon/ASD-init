407d5083f84648e5ae381c3f85bd3435c453b2ec
waltershalon <waltershalon@gmail.com>
2025-02-27 17:52:09 -0700

    Interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -9,13 +9,28 @@ export interface ConversationRequest {
     input: { text: string };
     variables?: { [key: string]: any };
     optionalArgs?: { [key: string]: any };
+    personaId?: string;
 }
 
+// This is the key interface that needs to change
 export interface ConversationResponse {
-    input?: { text: string };
-    output: { text: string };
-    variables?: { [key: string]: any };
-    fallback?: boolean;
+    answer: string;
+    answerAvailable: boolean;
+}
+
+// This is the format for sending WebSocket responses back to Soul Machines
+export interface SMConversationResponse {
+    category: string;
+    kind: string;
+    name: string;
+    body: {
+        personaId: string;
+        variables?: { [key: string]: any };
+        response: {
+            answer: string;
+            answerAvailable: boolean;
+        }
+    }
 }
 
 export interface SessionData {
